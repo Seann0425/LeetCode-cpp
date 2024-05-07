cab22e1371134610c5171242a62047cef0975f8f
Seann <seanyu0425@gmail.com>
2024-05-07 09:45:27 +0800

    45 added

---
+++ finished/45. Jump Game II.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {
+    }
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ListNode(int x, ListNode *next) : val(x), next(next) {
+    }
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+class Solution {
+public:
+    int jump(vector<int> &nums) {
+        const auto N = nums.size();
+        int ans = 0;
+        size_t furthest = 0, edge = 0;
+        for (size_t i = 0; i < N - 1; i++) {
+            furthest = max(furthest, static_cast<size_t>(nums[i]) + i);
+            if (furthest >= N - 1) return ++ans;
+            if (i == edge) {
+                edge = furthest;
+                ans++;
+            }
+        }
+        return 0;
+    }
+};
