aad73456d483be5553a4fd70a13f06735b740831
Seann <seanyu0425@gmail.com>
2024-09-03 11:42:08 +0800

    739

--- finished/739. Daily Temperatures.cpp
+++ finished/739. Daily Temperatures.cpp
@@ -47,14 +47,30 @@ public:
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int> &temperatures) {
-        vector<int> res(temperatures.size(), 0);
-        stack<int> monotonic;
-        for (int i = res.size() - 1; i >= 0; i--) {
+        const auto n = temperatures.size();
+        vector<int> res(n, 0);
+        stack<size_t> monotonic;
+        for (size_t i = n - 1; i < n; i--) {
             while (!monotonic.empty() && temperatures[monotonic.top()] <= temperatures[i])
                 monotonic.pop();
-            if (!monotonic.empty()) res[i] = monotonic.top() - i;
+            if (!monotonic.empty()) res[i] = static_cast<int>(monotonic.top() - i);
             monotonic.push(i);
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int> &temperatures) {
+//         vector<int> res(temperatures.size(), 0);
+//         stack<int> monotonic;
+//         for (int i = res.size() - 1; i >= 0; i--) {
+//             while (!monotonic.empty() && temperatures[monotonic.top()] <= temperatures[i])
+//                 monotonic.pop();
+//             if (!monotonic.empty()) res[i] = monotonic.top() - i;
+//             monotonic.push(i);
+//         }
+//         return res;
+//     }
+// };
