428a1e48642477d8968bc167a84138197b31935e
Seann <seanyu0425@gmail.com>
2024-09-13 10:27:09 +0800

    1310

---
+++ finished/1310. XOR Queries of a Subarray.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int> &arr, vector<vector<int>> &queries) {
+        const auto n = arr.size(), m = queries.size();
+        vector<int> prefix(n + 1), ans(m);
+        for (size_t i = 1; i <= n; i++) prefix[i] = prefix[i - 1] ^ arr[i - 1];
+        for (size_t i = 0; i < m; i++)
+            ans[i] = prefix[static_cast<size_t>(queries[i].back() + 1)] ^
+                     prefix[static_cast<size_t>(queries[i].front())];
+        return ans;
+    }
+};
