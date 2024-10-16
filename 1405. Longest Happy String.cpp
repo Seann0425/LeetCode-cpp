045c3b7e8fd0946a5194b5efa1e54480dcbc1e1e
Seann <seanyu0425@gmail.com>
2024-10-16 10:11:43 +0800

    1405

---
+++ 1405. Longest Happy String.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        string ans{};
+        priority_queue<pair<int, char>> pq;
+        if (a) pq.emplace(a, 'a');
+        if (b) pq.emplace(b, 'b');
+        if (c) pq.emplace(c, 'c');
+        while (pq.size() > 1) {
+            auto [cnt1, ch1] = pq.top();
+            pq.pop();
+            auto [cnt2, ch2] = pq.top();
+            pq.pop();
+                }
+    }
+};
