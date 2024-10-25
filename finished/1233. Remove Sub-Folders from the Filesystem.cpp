df0928bb9165881aa09379ea0f2ea5e64010910b
Seann <seanyu0425@gmail.com>
2024-10-25 09:53:52 +0800

    1233

--- finished/1233. Remove Sub-Folders from the Filesystem.cpp
+++ finished/1233. Remove Sub-Folders from the Filesystem.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -44,23 +38,53 @@ public:
 
 /*the solution should start from below*/
 
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string> &folder) {
-        const int n = static_cast<int>(folder.size());
+        const auto n = folder.size();
         sort(folder.begin(), folder.end());
-
-        string prev = folder[0] + "/"s;
         vector<string> ans;
-        ans.push_back(folder[0]);
-        for (int i = 1; i < n; i++) {
-            string tmp = folder[i] + "/";
+        ans.reserve(n);
+
+        string prev = folder.front() + "/"s;
+        ans.push_back(std::move(folder.front()));
+        for (size_t i = 1; i < n; i++) {
+            string tmp = folder[i] + "/"s;
             if (tmp.starts_with(prev)) continue;
             else {
-                ans.push_back(folder[i]);
+                ans.push_back(std::move(folder[i]));
                 prev = tmp;
             }
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<string> removeSubfolders(vector<string> &folder) {
+//         const int n = static_cast<int>(folder.size());
+//         sort(folder.begin(), folder.end());
+
+//         string prev = folder[0] + "/"s;
+//         vector<string> ans;
+//         ans.push_back(folder[0]);
+//         for (int i = 1; i < n; i++) {
+//             string tmp = folder[i] + "/";
+//             if (tmp.starts_with(prev)) continue;
+//             else {
+//                 ans.push_back(folder[i]);
+//                 prev = tmp;
+//             }
+//         }
+//         return ans;
+//     }
+// };
