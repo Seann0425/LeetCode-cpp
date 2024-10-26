9afec634c8032d0a7aec1cc624e2cec67a219b44
Seann <seanyu0425@gmail.com>
2024-10-26 08:34:44 +0800

    2352

---
+++ finished/2352. Equal Row and Column Pairs.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int equalPairs(vector<vector<int>> &grid) {
+        map<vector<int>, int> arrays;
+        auto ans = 0;
+        const auto n = grid.size();
+        vector<int> dummy(n);
+        for (const auto &v : grid) arrays[v]++;
+        for (size_t j = 0; j < n; j++) {
+            for (size_t i = 0; i < n; i++) dummy[i] = grid[i][j];
+            ans += arrays[dummy];
+        }
+        return ans;
+    }
+};
