9a895e38241da4082e47f23faba2817244fa5bce
Seann <seanyu0425@gmail.com>
2024-11-09 13:39:22 +0800

    1004

---
+++ finished/1004. Max Consecutive Ones III.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int longestOnes(vector<int> &nums, int k) {
+        const auto n = nums.size();
+        auto zero_cnt = 0;
+        auto ans = 0uz;
+        for (size_t l = 0, r = 0; r < n; r++) {
+            if (!nums[r]) zero_cnt++;
+            while (zero_cnt > k)
+                if (!nums[l++]) zero_cnt--;
+            if (r >= l) ans = max(ans, r - l + 1);
+        }
+        return static_cast<int>(ans);
+    }
+};
