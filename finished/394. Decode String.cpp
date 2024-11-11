6255aa030680d4f5fafd95b1f0a68b2107e1478f
Seann <seanyu0425@gmail.com>
2024-11-11 20:59:32 +0800

    394

---
+++ finished/394. Decode String.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+    size_t i = 0;
+public:
+    string decodeString(string &s) {
+        string ans{};
+        auto count = 0;
+        while (i < s.length()) {
+            auto c = s[i++];
+            if (isdigit(c)) count = count * 10 + (c - '0');
+            else if (c == '[') {
+                auto tmp = decodeString(s);
+                while (count--) ans += tmp;
+                count = 0;
+            } else if (c == ']') break;
+            else ans += c;
+        }
+        return ans;
+    }
+};
