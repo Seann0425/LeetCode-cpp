f4a679b2af422adfc8ef66f4b59d769b571a5391
Seann <seanyu0425@gmail.com>
2024-11-18 12:15:22 +0800

    1652

--- finished/1652. Defuse the Bomb.cpp
+++ finished/1652. Defuse the Bomb.cpp
@@ -4,24 +4,18 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
@@ -47,23 +41,44 @@ public:
 class Solution {
 public:
     vector<int> decrypt(vector<int> &code, int k) {
-        int n = code.size();
-        vector<int> de(n);
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            if (k >= 0)
-                for (int j = 1; j <= k; j++)
-                    sum += code[(i + j) % n];
-            else {
-                for (int j = -1; j >= k; j--) {
-                    int idx = i + j;
-                    while (idx < 0)
-                        idx += n;
-                    sum += code[idx];
-                }
-            }
-            de[i] = sum;
+        const auto n = code.size();
+        if (!k) return vector<int>(n, 0);
+        vector<int> res(n, 0);
+        size_t window;
+        if (k > 0) {
+            window = static_cast<size_t>(k);
+            for (size_t i = 1; i <= window; i++) res[0] += code[i];
+            for (size_t i = 1; i < n; i++) res[i] = res[i - 1] - code[i] + code[(i + window) % n];
+        } else {
+            window = static_cast<size_t>(-k);
+            for (size_t i = n - 2; i >= n - window - 1 && i < n; i--) res[n - 1] += code[i];
+            for (size_t i = n - 2; i < n; i--)
+                res[i] = res[i + 1] - code[i] + code[(i - window + n) % n];
         }
-        return de;
+        return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> decrypt(vector<int> &code, int k) {
+//         int n = code.size();
+//         vector<int> de(n);
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0;
+//             if (k >= 0)
+//                 for (int j = 1; j <= k; j++)
+//                     sum += code[(i + j) % n];
+//             else {
+//                 for (int j = -1; j >= k; j--) {
+//                     int idx = i + j;
+//                     while (idx < 0)
+//                         idx += n;
+//                     sum += code[idx];
+//                 }
+//             }
+//             de[i] = sum;
+//         }
+//         return de;
+//     }
+// };
