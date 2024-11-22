9ac7d00c13dc4191baa2f7e12a48b80974645a4f
Seann <seanyu0425@gmail.com>
2024-11-23 00:33:15 +0800

    230

--- finished/108. Convert Sorted Array to Binary Search Tree.cpp
+++ finished/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -4,50 +4,45 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 /*the solution should start from below*/
 
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
 class Solution {
+    vector<int> nums;
+    size_t n;
+    TreeNode *get_node(size_t left, size_t right) {
+        if (left > right or right >= n) return nullptr;
+        size_t mid = left + (right - left) / 2;
+        TreeNode *node = new TreeNode(nums[mid]);
+        node->left = get_node(left, mid - 1);
+        node->right = get_node(mid + 1, right);
+        return node;
+    }
 public:
     TreeNode *sortedArrayToBST(vector<int> &nums) {
-        TreeNode *root = new TreeNode;
-        if (nums.size() == 1) {
-            root->val = nums[0];
-            return root;
-        }
-        if (nums.size() == 2) {
-            root->val = nums[1];
-            TreeNode *left = new TreeNode;
-            left->val = nums[0];
-            root->left = left;
-            return root;
-        }
-        int mid = nums.size() / 2;
-        root->val = nums[mid];
-        vector<int> leftTree, rightTree;
-        leftTree.assign(nums.begin(), nums.begin() + mid);
-        rightTree.assign(nums.begin() + (mid + 1), nums.end());
-        root->left = sortedArrayToBST(leftTree);
-        root->right = sortedArrayToBST(rightTree);
-        return root;
+        this->nums = std::move(nums);
+        n = this->nums.size();
+        return get_node(0, n - 1);
     }
 };
