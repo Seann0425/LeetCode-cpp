d6609f0488d40981d76e1983d6de32d3a3af5b20
Seann <seanyu0425@gmail.com>
2024-11-23 01:28:48 +0800

    update

--- finished/5. Longest Palindromic Substring.cpp
+++ finished/5. Longest Palindromic Substring.cpp
@@ -4,43 +4,72 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 /*the solution should start from below*/
 
+// manacher
 class Solution {
 public:
     string longestPalindrome(string s) {
-        const int n = static_cast<int>(s.size());
-        vector<vector<bool>> Palindrome(n, vector<bool>(n, true));
-        for (int diff = 1; diff < n; diff++) {
-            for (int i = 0; i + diff < n; i++) {
-                Palindrome[i][i + diff] = (Palindrome[i + 1][i + diff - 1] && s[i] == s[i + diff]);
+        // preprocess
+        string preprocess = "$#";
+        for (const auto &c : s) {
+            preprocess += c;
+            preprocess += "#";
+        }
+        preprocess += "^";
+
+        // manacher
+        const auto n = preprocess.size();
+        vector<int> radius(n, 0);
+        auto left = 1uz, right = 1uz;
+        for (auto i = 1uz; i < n - 1; i++) {
+            radius[i] = 0, min(static_cast<int>(right - i), radius[left + (right - i)]);
+            while (preprocess[i + radius[i] + 1] == preprocess[i - radius[i] - 1]) radius[i]++;
+            if (i + radius[i] > right) {
+                left = i - radius[i];
+                right = i + radius[i];
             }
         }
-        for (int length = n; length > 0; length--) {
-            for (int begin = 0; begin + length <= n; begin++) {
-                if (Palindrome[begin][begin + length - 1]) return s.substr(begin, length);
-            }
-        }
-        return "";
+        auto max_radius = max_element(radius.begin(), radius.end());
+        auto center = static_cast<size_t>(max_radius - radius.begin());
+        auto ans = ""s;
+        for (auto i = center - *max_radius; i <= center + *max_radius; i++)
+            if (preprocess[i] != '#') ans += preprocess[i];
+        return ans;
     }
 };
+
+// dp
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         const int n = static_cast<int>(s.size());
+//         vector<vector<bool>> Palindrome(n, vector<bool>(n, true));
+//         for (int diff = 1; diff < n; diff++) {
+//             for (int i = 0; i + diff < n; i++) {
+//                 Palindrome[i][i + diff] = (Palindrome[i + 1][i + diff - 1] && s[i] == s[i +
+//                 diff]);
+//             }
+//         }
+//         for (int length = n; length > 0; length--) {
+//             for (int begin = 0; begin + length <= n; begin++) {
+//                 if (Palindrome[begin][begin + length - 1]) return s.substr(begin, length);
+//             }
+//         }
+//         return "";
+//     }
+// };
--- finished/70. Climbing Stairs.cpp
+++ finished/70. Climbing Stairs.cpp
@@ -4,38 +4,30 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode() : val(0), next(nullptr) {
-    }
-    ListNode(int x) : val(x), next(nullptr) {
-    }
-    ListNode(int x, ListNode *next) : val(x), next(next) {
-    }
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
-    }
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
-    }
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 /*the solution should start from below*/
 
+constexpr size_t max_n = 46;
+constexpr array<int, max_n> fib_seq = []() consteval {
+    array<int, max_n> fib_seq{1, 1};
+    for (size_t i = 2; i < max_n; i++) fib_seq[i] = fib_seq[i - 1] + fib_seq[i - 2];
+    return fib_seq;
+}();
+
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n == 1 || n == 2) return n;
-        vector<int> F(n);
-        F[0] = 1;
-        F[1] = 2;
-        for (int i = 2; i < n; i++) {
-            F[i] = F[i - 1] + F[i - 2];
-        }
-        return F[n - 1];
-    }
+    int climbStairs(size_t n) { return fib_seq[n]; }
 };
