9af7810711717242d9bfb18ba46719951307bb2c
Seann <seanyu0425@gmail.com>
2024-12-04 08:49:01 +0800

    2825

---
+++ finished/2825. Make String a Subsequence Using Cyclic Increments.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        const auto n1 = str1.size(), n2 = str2.size();
+        if (n1 < n2) return false;
+        for (auto i = 0uz, j = 0uz; i < n1; i++) {
+            if (str1[i] == str2[j] or str2[j] - str1[i] == 1 or str1[i] - str2[j] == 25) j++;
+            if (j == n2) return true;
+        }
+        return false;
+    }
+};
