acafba6bbd164c91661d1b2554f796bbf4d19a31
Seann <seanyu0425@gmail.com>
2024-12-06 11:36:21 +0800

    2554

---
+++ finished/2554. Maximum Number of Integers to Choose From a Range I.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int maxCount(vector<int> &banned, int n, int maxSum) {
+        sort(banned.begin(), banned.end());
+        auto ans = 0;
+        auto sum = 0ll;
+        auto num = 0uz;
+        const auto m = banned.size();
+        for (auto i = 1; i <= n; i++) {
+            while (num < m and banned[num] < i) num++;
+            if (num < m and banned[num] == i) continue;
+            if ((sum += i) > maxSum) break;
+            ans++;
+        }
+        return ans;
+    }
+};
