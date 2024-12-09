96bda4bc28b8816e9b9babdd60f23722ea0f8ee2
Seann <seanyu0425@gmail.com>
2024-12-09 23:40:28 +0800

    63

---
+++ finished/63. Unique Paths II.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &grid) {
+        const auto m = grid.size(), n = grid[0].size();
+        if (grid[0][0] or grid[m - 1][n - 1]) return 0;
+        grid[0][0] = 1;
+
+        for (size_t i = 0; i < m; i++)
+            for (size_t j = 0; j < n; j++) {
+                if (!i and !j) continue;
+                if (grid[i][j]) grid[i][j] = 0;
+                else grid[i][j] = (i ? grid[i - 1][j] : 0) + (j ? grid[i][j - 1] : 0);
+            }
+
+        return grid[m - 1][n - 1];
+    }
+};
