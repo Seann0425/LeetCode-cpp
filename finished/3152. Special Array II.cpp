d95b045be98de69b458ef59a411255fa45f01092
Seann <seanyu0425@gmail.com>
2024-12-09 09:45:34 +0800

    3152

---
+++ finished/3152. Special Array II.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int> &nums, vector<vector<int>> &queries) {
+        const auto n = nums.size();
+        vector<int> invalid;
+        invalid.reserve(n);
+        for (size_t i = 0; i < n - 1; i++)
+            if (!((nums[i] ^ nums[i + 1]) & 1)) invalid.push_back(i);
+        vector<bool> ans;
+        ans.reserve(queries.size());
+        for (const auto &q : queries) {
+            auto it = lower_bound(invalid.begin(), invalid.end(), q[0]);
+            ans.push_back(it == invalid.end() or *it >= q[1]);
+        }
+        return ans;
+    }
+};
