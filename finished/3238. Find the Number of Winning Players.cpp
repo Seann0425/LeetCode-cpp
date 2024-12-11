857601aa7cb42ba18d2f89d6e1934a62a15e6370
Seann <seanyu0425@gmail.com>
2024-12-11 16:11:01 +0800

    3238

---
+++ finished/3238. Find the Number of Winning Players.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int winningPlayerCount(int n, vector<vector<int>> &pick) {
+        array<array<int, 12>, 10> pick_balls;
+        for (const auto &p : pick) pick_balls[p[0]][p[1]]++;
+        auto ans = 0;
+        for (int i = 0; i < 10; i++)
+            for (const auto &b : pick_balls[i])
+                if (b > i) {
+                    ans++;
+                    break;
+                }
+        return ans;
+    }
+};
