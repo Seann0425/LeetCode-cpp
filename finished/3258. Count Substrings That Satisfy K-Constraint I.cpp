ffba08c311d513f34fe191db5439fcc145506eed
Seann <seanyu0425@gmail.com>
2024-12-11 15:48:54 +0800

    3258

---
+++ finished/3258. Count Substrings That Satisfy K-Constraint I.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int countKConstraintSubstrings(string s, int k) {
+        const auto n = s.size();
+        auto zero_count = 0, one_count = 0;
+        auto ans = 0uz;
+        for (auto l = 0uz, r = 0uz; r < n; r++) {
+            (s[r] == '0' ? zero_count : one_count)++;
+            while (min(zero_count, one_count) > k) (s[l++] == '0' ? zero_count : one_count)--;
+            ans += r - l + 1;
+        }
+        return static_cast<int>(ans);
+    }
+};
