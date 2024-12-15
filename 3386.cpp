8c7759ef2e207ece8d8c8335f827d67e9a36d06e
Seann <seanyu0425@gmail.com>
2024-12-15 12:04:13 +0800

    final exam QQ

---
+++ 3386.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+/*the solution should start from below*/
+
+// #pragma GCC optimize("O3", "unroll-loops")
+// static const auto InitialOptimization = [](){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 0;
+// }();
+
+class Solution {
+public:
+    int buttonWithLongestTime(vector<vector<int>> &events) {
+        auto prev = 0, ans = 0, max_time = 0;
+        for (const auto &e : events) {
+            if (e[1] - prev > max_time) {
+                max_time = e[1] - prev;
+                ans = e[0];
+            } else if (e[1] - prev == max_time) {
+                ans = min(ans, e[0]);
+            }
+            prev = e[1];
+        }
+        return ans;
+    }
+};
